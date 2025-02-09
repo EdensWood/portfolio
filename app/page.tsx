ef46fc843cddf01ecf9ae30a6c56baf7c502bf2e
EdensWood <woodsleafy@gmail.com>
2025-02-09 13:52:01 +0300

    second-commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main
+      className={
+        "relative mx-auto flex flex-col items-center justify-center bg-black-100 px-5 sm:px-10 overflow-clip"
+      }
+    >
+      <div className={"w-full max-w-7xl"}>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
